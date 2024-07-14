3382c877d86acbbbb729da533aa6d228ea55f10e
affanjkhan <affanjkhan15@gmail.com>
2024-07-14 17:39:24 -0400

    menu.go

--- menu.go
+++ menu.go
@@ -13,17 +13,17 @@ type Menu struct {
 
 var menu = []Menu{
 	//itemNo  itemName   itemPrice
-	{1, "Adrakh Chai", 20},
-	{2, "Filter Coffee", 25},
-	{3, "Chhas", 35.50},
-	{4, "Lassi", 30},
-	{5, "Mango Lassi", 60},
-	{6, "Kadi Chaawal", 50},
-	{7, "Chhole Bhature", 45},
-	{8, "Khasta Kachori", 30},
-	{9, "Raj Kachori", 30},
+	{1, "Burger", 20},
+	{2, " Coffee", 25},
+	{3, "Tea", 35.50},
+	{4, "Yogurt", 30},
+	{5, "Apples", 60},
+	{6, "Rice", 50},
+	{7, "Mangoes", 45},
+	{8, "Bread", 30},
+	{9, "eggs", 30},
 	{10, "Veg. Sandwich", 20},
-	{11, "Veg. Masala Maggi", 60.00},
+	{11, "Noodles", 60.00},
 	{12, "Samosa", 20},
 	{13, "Cream Roll", 15},
 }
